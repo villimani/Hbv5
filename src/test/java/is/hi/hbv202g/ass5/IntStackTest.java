e0a8e259f061f704fa720c6ef042dc8217b2b821
villimani <vilmundurmani@gmail.com>
2023-02-24 00:44:22 +0000

    Test cases

--- src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
+++ src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
@@ -5,13 +5,47 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class IntStackTest {
+private IntStack empty;
+    @Before
+    public void constructStackTestObjects(){
+        empty=new IntStack();
+    }
 
     @Test
     public void testNewStackIsNotFull() {
-        IntStack stack= new IntStack();
-
+        IntStack stack= empty;
         assertFalse(stack.isFull());
+    }
+    @Test
+    public void testFullStackIsFull(){
+        IntStack stack= empty;
+        for (int i=1;i< stack.getCapacity();i++){
+            stack.push(1);
+        }
+        assertTrue(stack.isFull());
+    }
+
+    @Test
+    public void testPopReturnsPushedValue(){
+        IntStack stack=empty;
+        int expected= 1;
+        stack.push(1);
+        assertEquals(expected,stack.pop());
+    }
+
+    @Test(expected=ArithmeticException.class)
+    public  void testPopIsAllowed(){
+        IntStack stack =empty;
+        stack.pop();
+    }
+    @Test(expected=ArithmeticException.class)
+    public  void testPushIsAllowed(){
+        IntStack stack =empty;
+        for (int i=1;i<= stack.getCapacity();i++){
+            stack.push(1);
+        }
     }
 
 }
